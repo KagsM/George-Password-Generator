6cf38370053e1428b74e390fb9e097f21cb596e0
Malcolm <kagsmalcolm@gmail.com>
2025-07-10 07:29:42 +0300

    Complete password generator

--- index.js
+++ index.js
@@ -20,8 +20,31 @@ function passwordGenerator(charsQuantity){
     ];
 
     const randomChar = (charset) =>
-        charset(Math.floor(Math.random()*charset.length));//Check this
+        charset[Math.floor(Math.random()*charset.length)];//Check this
+
+    const shuffle = (array) => array.sort(() => Math.random()-0.5);
+
+    const selectedCategories = shuffle([...categories]).slice(0.3);
+
+    let passwordChars = [];
+
+    for (const category of selectedCategories){passwordChars.push(randomChar(category.chars));}
+
+    const pool = selectedCategories.map(cat => cat.chars).join('');
+
+    for(let i = passwordChars.length; i < charsQuantity; i++){
+        passwordChars.push(randomChar(pool));
+    }
+
+    return shuffle(passwordChars).join('');
 }
+
+console.log(passwordGenerator(null));
+console.log(passwordGenerator('abc'));
+console.log(passwordGenerator(2));
+console.log(passwordGenerator(16));
+console.log(passwordGenerator(8));
+
 //Random numbers of characters
 //Characters are chosen at random
 //At least one character is chosen
